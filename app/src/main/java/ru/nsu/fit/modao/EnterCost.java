6bdc6b9c265e32565d9e376a5e72cafc4eb75bf9
Olga <o.pogibelnaya@g.nsu.ru>
2022-11-30 02:17:02 +0700

    создание странички ввода стоимости траты

---
+++ app/src/main/java/ru/nsu/fit/modao/EnterCost.java
@@ -0,0 +1,39 @@
+package ru.nsu.fit.modao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class EnterCost extends AppCompatActivity {
+    ImageButton expensesButton;
+    ImageButton groupsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        expensesButton = findViewById(R.id.buttonExpenses);
+        expensesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewActivity(ExpensesActivity.class);
+            }
+        });
+
+        groupsButton = findViewById(R.id.buttonGroups);
+        groupsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNewActivity(GroupsActivity.class);
+            }
+        });
+    }
+    void startNewActivity(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+    }
+}
