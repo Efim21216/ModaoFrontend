28ebe3030e6a1677308419fee1b09cc79c9d80ed
Efim21216 <e.pashko@g.nsu.ru>
2023-02-05 14:24:36 +0700

    Начал переделывать на фрагменты

---
+++ app/src/main/java/ru/nsu/fit/modao/MasterActivity.java
@@ -0,0 +1,25 @@
+package ru.nsu.fit.modao;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MasterActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+
+        bottomNavigationView = findViewById(R.id.bottomMenu);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
