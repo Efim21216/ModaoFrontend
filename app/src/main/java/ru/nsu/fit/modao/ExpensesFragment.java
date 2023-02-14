7ef51fe9a0414081a01f8b33df9cfc0d84028cec
Olga <o.pogibelnaya@g.nsu.ru>
2023-02-14 20:07:23 +0700

    Сделала что-то похожее на подключение адаптера в ExpensesFragment

--- app/src/main/java/ru/nsu/fit/modao/ExpensesFragment.java
+++ app/src/main/java/ru/nsu/fit/modao/ExpensesFragment.java
@@ -1,19 +1,19 @@
 package ru.nsu.fit.modao;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ru.nsu.fit.modao.adapter.ExpensesAdapter;
+import ru.nsu.fit.modao.databinding.FragmentExpensesBinding;
 import ru.nsu.fit.modao.model.Currency;
 import ru.nsu.fit.modao.model.Expenses;
 
@@ -32,9 +32,9 @@ public class ExpensesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    List<Expenses> expensesList = new ArrayList<>();
-    RecyclerView expensesRecycler;
+    FragmentExpensesBinding binding;
     ExpensesAdapter expensesAdapter;
+    List<Expenses> expensesList = new ArrayList<>();
 
     public ExpensesFragment() {
         // Required empty public constructor
@@ -69,25 +69,26 @@ public class ExpensesFragment extends Fragment {
         expensesList.add(new Expenses(-900, "Nikita paid", "Nikita Paid", Currency.RUB));
         expensesList.add(new Expenses(1200, "You paid", "You paid", Currency.RUB));
         expensesList.add(new Expenses(100, "Olga paid", "Olga Paid", Currency.RUB));
-
-        setExpensesRecycler(expensesList);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentExpensesBinding.inflate(inflater, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expenses, container, false);
+        return binding.getRoot();
     }
 
-    private void setExpensesRecycler(List<Expenses> expensesList) {
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
-        expensesRecycler = findViewById(R.id.expensesRecycler);
-        // Установить настройки
-        expensesRecycler.setLayoutManager(layoutManager);
-        // Вся логика подстановки, которую писали раньше
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         expensesAdapter = new ExpensesAdapter(getContext(), expensesList);
-        // Установка адаптера
-        expensesRecycler.setAdapter(expensesAdapter);
+        binding.expensesRecycler.setAdapter(expensesAdapter);
     }
 }
