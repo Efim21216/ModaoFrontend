94fd6d6927239a1b892c1bcf48941295a94610e5
Olga <o.pogibelnaya@g.nsu.ru>
2023-03-01 02:19:16 +0700

    Наброски на список друзей

--- app/src/main/java/ru/nsu/fit/modao/fragments/FriendsFragment.kt
+++ app/src/main/java/ru/nsu/fit/modao/fragments/FriendsFragment.kt
@@ -6,10 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.nsu.fit.modao.databinding.FragmentFriendsBinding
+import ru.nsu.fit.modao.models.User
 
 class FriendsFragment: Fragment() {
     private var _binding: FragmentFriendsBinding? = null
     private val binding get() = _binding!!
+    //private val adapter: ExpensesAdapter = ExpensesAdapter()
+    private val friends: Array<User> = arrayOf(
+            User(username = "Olga", phone_number = "89009001010", bank = "Bank1"),
+            User(username = "Efim", phone_number = "89009001020", bank = "Bank2"),
+            User(username = "Petr", phone_number = "89009001030", bank = "Bank1"),
+            User(username = "Nikita", phone_number = "89009001040", bank = "Bank2")
+    )
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFriendsBinding.inflate(inflater, container, false)
         return binding.root
