1852168f6af19f1875e765beff0fdaef6bede513
Olga <o.pogibelnaya@g.nsu.ru>
2023-03-01 12:34:15 +0700

    Подправили GroupMembersFragment.kt

--- app/src/main/java/ru/nsu/fit/modao/fragments/GroupMembersFragment.kt
+++ app/src/main/java/ru/nsu/fit/modao/fragments/GroupMembersFragment.kt
@@ -2,30 +2,30 @@ package ru.nsu.fit.modao.fragments
 
 import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import ru.nsu.fit.modao.adapter.SelectAdapter
+import ru.nsu.fit.modao.adapter.AdapterListener
+import ru.nsu.fit.modao.adapter.FriendsAdapter
 import ru.nsu.fit.modao.databinding.FragmentGroupMembersBinding
-import ru.nsu.fit.modao.models.ParticipantEvent
+import ru.nsu.fit.modao.models.User
 import ru.nsu.fit.modao.repository.Repository
 import ru.nsu.fit.modao.utils.App
 import ru.nsu.fit.modao.viewmodels.MainViewModel
 import ru.nsu.fit.modao.viewmodels.RepositoryViewModelFactory
 
-class GroupMembersFragment : Fragment(), SelectAdapter.CustomListener {
+class GroupMembersFragment : Fragment(), AdapterListener<User> {
     private var _binding: FragmentGroupMembersBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainViewModel: MainViewModel
     private lateinit var app: App
     private val args by navArgs<GroupMembersFragmentArgs>()
-    private val adapter = SelectAdapter()
+    private val adapter = FriendsAdapter()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -44,13 +44,13 @@ class GroupMembersFragment : Fragment(), SelectAdapter.CustomListener {
         val viewModelFactory = RepositoryViewModelFactory(repository)
         mainViewModel = ViewModelProvider(requireActivity(), viewModelFactory)[MainViewModel::class.java]
 
-        adapter.attachListener(this)
+        adapter.setListener(this)
         binding.recyclerMembers.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-        val list = mainViewModel.usersInGroup.value?.map {user -> ParticipantEvent(username = user.username) } ?: listOf<ParticipantEvent>()
-        adapter.setList(list.toTypedArray())
+        val list : Array<User> = mainViewModel.usersInGroup.value ?: arrayOf()
+        adapter.setFriendsList(list)
         binding.recyclerMembers.adapter = adapter
         mainViewModel.usersInGroup.observe(viewLifecycleOwner){
-            adapter.setList(it.map {user -> ParticipantEvent(username = user.username) }.toTypedArray())
+            adapter.setFriendsList(it)
         }
         mainViewModel.getUsersInGroup(args.group.id!!)
         binding.buttonAdd.setOnClickListener {
@@ -69,9 +69,12 @@ class GroupMembersFragment : Fragment(), SelectAdapter.CustomListener {
 
 
     }
-
-    override fun onClickItem(button: CheckBox, participantEvent: ParticipantEvent) {
-        TODO("Not yet implemented")
+    override fun onClickItem(item: User) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(item.username)
+        builder.setMessage("Phone: " + item.phone_number + "\n" + "Bank: " + item.bank)
+        builder.setPositiveButton("OK") { _, _ -> }
+        builder.create().show()
     }
 
 }
