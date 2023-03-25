01a8e182da899cc4c33dbc9a8cb7bad030bf481f
Olga <o.pogibelnaya@g.nsu.ru>
2023-03-25 21:51:01 +0700

    Замена фрагмента Group Expenses на Events

---
+++ app/src/main/java/ru/nsu/fit/modao/fragments/CreateAnExpenseFragment.java
@@ -0,0 +1,26 @@
+package ru.nsu.fit.modao.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.nsu.fit.modao.R;
+
+public class CreateAnExpenseFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_an_expense, container, false);
+    }
+}
